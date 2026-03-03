a303b13d09b3ae2a7fef3321090b55556c30fa30
Luisa Hernandez Rendon <luisa.hernandez@flyr.com>
2026-03-03 22:23:30 +0100

    add description person

--- js/variables.js
+++ js/variables.js
@@ -1,2 +1,22 @@
 console.log("Variables in JavaScript");
 
+//¿que es una variable? es un espacio reservado en memoria, donde guardamos el valor para usarse despues como strings, numeros, booleanos
+
+let age = 18; //guardar valores que a futuro van a cambiar
+
+const firstName = "Luisa"; //guardar valores que no van a cambiar en el tiempo y no podra reasignarse
+
+//camelcase siempre para las variables, es decir minus y segunda palabra mayus y todo seguido
+//nombre descriptivo y acertado a lo que queremos guardar en ella
+
+//Ejercicio - declarar una variable que describa a una persona e imprimir en consola
+
+const nombre = "Luisa";
+let edad = 34;
+let peso = 55;
+let estatura = 1.63;
+const isFemenine = true;
+
+console.log("esta es la descripcion es: ", nombre,"su edad es:", edad, "su peso es: ", peso, "su estatura es: ", estatura, "su genero es: ",isFemenine)
+
+
