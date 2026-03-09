992824a2b54567c05378c77a8fa30b9129bf4392
Luisa Hernandez Rendon <luisa.hernandez@flyr.com>
2026-03-09 21:52:28 +0100

    Add explanation arrow in archive

--- js/functionArrow.js
+++ js/functionArrow.js
@@ -1,1 +1,24 @@
 console.log("explicacion funcion flecha");
+
+//esto es lo mismo que una función, cual es la diferencia entre el arrow y una normal? pues la sintaxis
+
+function verifyIsMAjor(edad){
+    if(edad >= 18){
+        return true
+    } else{
+        return false
+    }
+}
+
+//con arrow
+
+const verifyIsMajor =(age)=>{
+    if(age >= 18){
+        return true
+    } else{
+    return false
+}
+}
+
+console.log("resultado desde una function",verifyIsMAjor(19));
+console.log("resultado desde una function con flecha",verifyIsMajor(67));
